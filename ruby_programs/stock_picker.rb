d798c97184fdb39d7a38ef295cf00ab52720b1d7
Angela Dazzle <angela@pixelgrinder.co.uk>
2022-08-15 11:31:18 +0100

    More example arrays added to stock_picker.rb Catches if no days are profitable

--- ruby_programs/stock_picker.rb
+++ ruby_programs/stock_picker.rb
@@ -4,7 +4,6 @@ def stock_picker(days)
   daily_stats = []
 
   days.each_with_index do |price, index|
-    
     daily_high = 0
     which_day = 0
     days.slice(index + 1...days.length).each_with_index do |compare, index|
@@ -14,25 +13,39 @@ def stock_picker(days)
         which_day = days.find_index(compare)
       end
     end
-    
     daily_stats << { buy_day: index, sell_day: which_day, dif: daily_high }
-    
   end
 
   current_max = 0
   daily_stats.each_with_index do |day, index|
-    
     if daily_stats[index][:dif] > current_max
       best_day = daily_stats[index]
       current_max = daily_stats[index][:dif]
     end
-
   end
 
-  puts "Buy day  : #{best_day[:buy_day] + 1}"
-  puts "Sell day : #{best_day[:sell_day] + 1}"
-  puts "Profit   : $#{best_day[:dif]}"
+  if best_day.empty? 
+    puts "********"
+    puts "Best not to trade in this period"
+    puts "********"
+  else 
+    puts "Buy day  : #{best_day[:buy_day] + 1}"
+    puts "Sell day : #{best_day[:sell_day] + 1}"
+    puts "Profit   : $#{best_day[:dif]}"
+  end
 
 end
 
+
+puts "----"
+puts "Period 1"
 stock_picker([17,3,6,9,15,8,6,1,10])
+puts "----"
+puts "Period 2"
+stock_picker([1,2,3,4,5,6,7,8,9,10])
+puts "----"
+puts "Period 3"
+stock_picker([10,9,8,7,6,5,4,3,2,1])
+puts "----"
+puts "Period 4"
+stock_picker([1,34,2,5,65,34,3,322])
