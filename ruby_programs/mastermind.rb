821b5c9442bca961f7050f693b0992aed2075e02
Angela Dazzle <angela@pixelgrinder.co.uk>
2022-09-21 06:22:08 +0100

    computer player with random guesses created

--- ruby_programs/mastermind.rb
+++ ruby_programs/mastermind.rb
@@ -1,23 +1,23 @@
 
-module Drawing
+# module Drawing
 
-  def draw_board(rounds)
+#   def draw_board(rounds)
 
-    i = 0
-    puts " ___ ___ ___ ___"
-    while i < rounds do
+#     i = 0
+#     puts " ___ ___ ___ ___"
+#     while i < rounds do
 
-      x = "r#{i + 1}".to_sym
-      y = "fb#{i + 1}".to_sym
-      puts "|   |   |   |   |"
-      puts "| #{@board[x][0]} | #{@board[x][1]} | #{@board[x][2]} | #{@board[x][3]} | #{@feedback[y].join}"
-      puts "|___|___|___|___|"
-      i += 1
-    end
-    puts " "
-  end
+#       x = "r#{i + 1}".to_sym
+#       y = "fb#{i + 1}".to_sym
+#       puts "|   |   |   |   |"
+#       puts "| #{@board[x][0]} | #{@board[x][1]} | #{@board[x][2]} | #{@board[x][3]} | #{@feedback[y].join}"
+#       puts "|___|___|___|___|"
+#       i += 1
+#     end
+#     puts " "
+#   end
 
-end
+# end
 
 class Board
 
@@ -51,16 +51,36 @@ class Board
     fb = "fb#{round}".to_sym
     guess.each_with_index { |val, ind| @board[r][ind] = val }
     feedback.each_with_index { |val, ind| @feedback[fb][ind] = val }
-  end
 
+    i = 0
+    puts " ___ ___ ___ ___"
+    while i < round do
+
+      x = "r#{i + 1}".to_sym
+      y = "fb#{i + 1}".to_sym
+      puts "|   |   |   |   |"
+      puts "| #{@board[x][0]} | #{@board[x][1]} | #{@board[x][2]} | #{@board[x][3]} | #{@feedback[y].join}"
+      puts "|___|___|___|___|"
+      i += 1
+    end
+    puts " "
+  end
   
 end
 
 class Game < Board
 
+  print "(C)omputer or (Human) code maker? "
+  maker = gets.chomp.upcase
+
   current_game = Board.new
-  the_code = Array.new(4) { rand(1..6) }
-  
+
+  if maker == "H" 
+    print "Enter a 4 digit code (1-6) : "
+    the_code = gets.chomp.slice(0,4)
+  else
+    the_code = Array.new(4) { rand(1..6) }
+  end
 
   escape = 0
   rounds = 1
@@ -69,35 +89,46 @@ class Game < Board
 
     feedback = []
 
-    print 'Please enter a 4 peg code (1-6) :'
-    guess = gets.chomp.split("").map! { |val| val.to_i }.first(4)
-    
-    puts "The code = #{the_code}"
+    if maker == "C"
+      print 'Please enter a 4 peg code (1-6) : '
+      guess = gets.chomp.split("").map! { |val| val.to_i }.first(4)
+      
+      puts "The code = #{the_code}"
 
-    guess.each_with_index do |val, ind|
-      if the_code[ind] == val
-        feedback << "Y"
-      else
-        feedback << val
+      guess.each_with_index do |val, ind|
+        if the_code[ind] == val
+          feedback << "Y"
+        else
+          feedback << val
+        end
       end
+
+      the_code.each_with_index do |val, ind|
+        if feedback[ind] != "Y" && feedback.include?(val)
+          feedback[feedback.find_index(val)] = "O"
+        end
+      end
+
+      feedback.map! do |val| 
+        if val == "O" || val == "Y"
+          val = val
+        else 
+          val = "-"
+        end
+      end
+      
+      current_game.update_board(rounds, guess, feedback)
+    else
+      guess = Array.new(4) { rand(1..6) }
+      
+      current_game.update_board(rounds, guess, feedback)
+      puts "The guess = #{guess}"
+      print "Please give feedback using Y (correct), O (matching number), or - (incorrect) : "
+      feedback = gets.chomp.upcase.split("")
+      
+      current_game.update_board(rounds, guess, feedback)
     end
 
-    the_code.each_with_index do |val, ind|
-      if feedback[ind] != "Y" && feedback.include?(val)
-        feedback[feedback.find_index(val)] = "O"
-      end
-    end
-
-    feedback.map! do |val| 
-      if val == "O" || val == "Y"
-        val = val
-      else 
-        val = "-"
-      end
-    end
-
-    current_game.update_board(rounds, guess, feedback)
-    current_game.draw_board(rounds)
     if feedback == ["Y","Y","Y","Y"] 
       puts "WINNER"
       escape = 1
@@ -108,10 +139,8 @@ class Game < Board
 
 end
 
-Game
+Game 
 
-# create computer player to guess the code
-# add ability to choose whether player is code maker or breaker
 # build my own approach for computer guessing
 # research & implement several strategies for solving & pick one at random each go, including my version
 # https://puzzling.stackexchange.com/questions/546/clever-ways-to-solve-mastermind
